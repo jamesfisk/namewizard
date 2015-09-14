2fb927fd7bf0aa308bee2289e7d837b960ecc060
James Fisk <jamesfisk@utexas.edu>
2015-09-14 14:48:57 -0500

    Minor copy change

--- namer/views.py
+++ namer/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpRequest
 from .models import Matrix, YEARS, GENDERS
 import cPickle
 import numpy as np
